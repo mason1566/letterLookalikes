659a253bb05a6f075e98c16dc616b4aa2091a2ce
Mason Johnson <273-C0514274@users.noreply.gitlab.compsci.camosun.ca>
2025-03-16 20:52:18 -0700

    add web interface for letter changing

---
+++ letterChanger/index.mjs
@@ -0,0 +1,14 @@
+
+
+/* Element Declarations */
+const content = document.getElementById("content");
+const userInput = document.getElementById("userInput");
+const letterChanger = document.getElementById("letterChanger");
+const outputInput = document.getElementById("outputInput");
+
+/** Content Component */
+content.style.display = "flex";
+
+/* Letter Changer Component */
+letterChanger.style.overflow = "scroll";
+
