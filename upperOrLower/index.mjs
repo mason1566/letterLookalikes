2c9d4506d009e06bd7f9e5f50c64dfe69fb9f60b
Mason Johnson <273-C0514274@users.noreply.gitlab.compsci.camosun.ca>
2025-03-16 20:05:59 -0700

    finish letter lookalikes

--- upperOrLower/index.mjs
+++ upperOrLower/index.mjs
@@ -7,7 +7,6 @@ catch (err) {
     throw err;
 }
 
-
 let currentVariantIndex = 0;
 let currentLetterIndex = 0;
 let letters = Object.keys(letterJson); // An array of letters
@@ -20,16 +19,24 @@ letters.forEach(letter => {
     newLetters[letter].upper = [];
     newLetters[letter].other = [];
 });
-console.log(newLetters);
+// console.log(newLetters);
 
 // HTML element references
 const textOutput = document.getElementById("textOutput");
 const lowerButton = document.getElementById("lowerButton");
 const upperButton = document.getElementById("upperButton");
 const otherButton = document.getElementById("otherButton");
+const deleteButton = document.getElementById("deleteButton");
 const letterSelect = document.getElementById("letterSelect");
 const renderJsonButton = document.getElementById("renderJsonButton");
 const jsonOutput = document.getElementById("jsonOutput");
+const liveUpdateCheckbox = document.getElementById("liveUpdateCheckbox");
+
+// Reset jsonOutput values based on previous states (browser caches these elements)
+if (liveUpdateCheckbox.checked)
+    jsonOutput.value = JSON.stringify(newLetters, null, 2);
+else
+    jsonOutput.value = "";
 
 // Create option elements for each letter and append them to letterSelect element
 letters.forEach((letter) => {
@@ -41,20 +48,35 @@ letters.forEach((letter) => {
 
 // Set the currentLetter to the selected option in the letterSelect element
 currentLetterIndex = letters.indexOf(letterSelect.selectedOptions[0].value);
-console.log(letters[currentLetterIndex]);
+// console.log(letters[currentLetterIndex]);
 textOutput.value = letterJson[letters[currentLetterIndex]].lower[currentVariantIndex];
 
 lowerButton.addEventListener('click', () => {
     let l = letters[currentLetterIndex];
-    console.log(l)
     newLetters[l].lower.push(letterJson[l].lower[currentVariantIndex]);
     goNextVariant();
 });
 
+upperButton.addEventListener('click', () => {
+    let l = letters[currentLetterIndex];
+    newLetters[l].upper.push(letterJson[l].lower[currentVariantIndex]);
+    goNextVariant();
+});
+
+otherButton.addEventListener('click', () => {
+    let l = letters[currentLetterIndex];
+    newLetters[l].other.push(letterJson[l].lower[currentVariantIndex]);
+    goNextVariant();
+});
+
+deleteButton.addEventListener('click', () => {
+    goNextVariant();
+})
+
 // Used to update the current letter (via the dropdown menu)
 letterSelect.addEventListener('change', (event) => {
     currentLetterIndex = letters.indexOf(event.target.selectedOptions[0].value);
-    textOutput.value = letters[currentLetterIndex];
+    goToLetter(letters[currentLetterIndex])
 });
 
 // Used to output the newLetters json object
@@ -62,27 +84,82 @@ renderJsonButton.addEventListener('click', () => {
     jsonOutput.value = JSON.stringify(newLetters, null, 2);
 });
 
+if (window.localStorage.textareaHeight && window.localStorage.textareaWidth) {
+    jsonOutput.style.height = `${window.localStorage.textareaHeight}px`;
+    jsonOutput.style.width = `${window.localStorage.textareaWidth}px`;
+}
+
+// Observe resizing of the textarea
+const textareaResizeObserver = new ResizeObserver((entry) => {
+    window.localStorage.textareaHeight = entry[0].contentRect.height;
+    window.localStorage.textareaWidth = entry[0].contentRect.width;
+})
+textareaResizeObserver.observe(jsonOutput);
+
+// Activate live updating of json output if checkbox is checked
+let liveUpdateActive = liveUpdateCheckbox.checked;
+liveUpdateCheckbox.addEventListener('change', () => {
+    liveUpdateActive = liveUpdateCheckbox.checked;
+    if (liveUpdateActive) 
+        jsonOutput.value = JSON.stringify(newLetters, null, 2);
+});
+
+function deleteCurrentVariant() {
+    letterJson[letters[currentLetterIndex]].lower.splice(currentVariantIndex, 1);
+}
+
 function goNextVariant() {
-    currentVariantIndex++;
-    if (currentVariantIndex >= letterJson[currentLetter].lower.length) 
-        goNextLetter();
-    textOutput.value = letterJson[letters[currentLetterIndex]]
+    // currentVariantIndex++;
+    deleteCurrentVariant();
+    if (currentVariantIndex >= letterJson[letters[currentLetterIndex]].lower.length) {
+        disableLetterButtons()
+        disableLetterOption(currentLetterIndex);
+        textOutput.value = "Done!";
+    }
+    else {
+        textOutput.value = letterJson[letters[currentLetterIndex]].lower[currentVariantIndex];
+    
+        if (liveUpdateActive) {
+            jsonOutput.value = JSON.stringify(newLetters, null, 2);
+        }
+    }
 }
 
-function goNextLetter() {
-    currentLetterIndex++;
+function goToLetter(letter) {
+    // Enable/disable buttons if there are no entries to give
+    let letterIndex = letters.indexOf(letter);
+    if (letterIndex == -1) 
+        throw new Error("Letter index out of range.");
+    currentLetterIndex = letterIndex;
     currentVariantIndex = 0;
-    if (currentLetterIndex >= letters.length) 
-        endSurvey();
 
+    if (letterJson[letters[currentLetterIndex]].lower.length <= 0) {
+        textOutput.value = "Done!";
+        disableLetterButtons();
+        disableLetterOption(currentLetterIndex);
+    }
+    else {
+        textOutput.value = letterJson[letters[currentLetterIndex]].lower[currentVariantIndex];
+        enableLetterButtons();
+    }
 }
 
-function endSurvey() {
-    textOutput.value = "Done!";
-
+function disableLetterButtons() {
     lowerButton.disabled = true;
     upperButton.disabled = true;
     otherButton.disabled = true;
 }
 
-console.log("Ready");
+function enableLetterButtons() {
+    lowerButton.disabled = false;
+    upperButton.disabled = false;
+    otherButton.disabled = false;
+}
+
+function disableLetterOption(index) {
+    letterSelect.children[index].disabled = true;
+}
+
+function endSurvey() {
+
+}
