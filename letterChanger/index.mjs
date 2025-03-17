914fd1a3fd331a9e9d122d1771870a3a844d2f26
Mason Johnson <273-C0514274@users.noreply.gitlab.compsci.camosun.ca>
2025-03-16 21:17:11 -0700

    add basic layout and functions

--- letterChanger/index.mjs
+++ letterChanger/index.mjs
@@ -1,14 +1,27 @@
-
-
 /* Element Declarations */
 const content = document.getElementById("content");
 const userInput = document.getElementById("userInput");
 const letterChanger = document.getElementById("letterChanger");
 const outputInput = document.getElementById("outputInput");
 
-/** Content Component */
+/* Content Component */
 content.style.display = "flex";
+content.style.width = "100%";
+content.style.alignItems = "flex-start";
 
 /* Letter Changer Component */
 letterChanger.style.overflow = "scroll";
+letterChanger.style.width = "100%";
 
+function setLetterChangerString(newString) {
+    letterChanger.innerHTML = newString;
+    setOutputInputString(newString);
+}
+
+/* Output Input Component */
+function setOutputInputString(newString) {
+    outputInput.value = newString
+}
+
+/* User Input Component */
+userInput.addEventListener('input', () => setLetterChangerString(userInput.value))
